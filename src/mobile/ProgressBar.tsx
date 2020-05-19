809d019e61257e277ddb8b2e85819f4971451b5f
kapil <kapilbhosale@hotmail.com>
2020-05-20 00:06:25 +0530

    progress bar set to initial height

--- src/mobile/ProgressBar.tsx
+++ src/mobile/ProgressBar.tsx
@@ -70,7 +70,7 @@ const styles = StyleSheet.create({
   },
   progress: {
     width: "100%",
-    height: 40,
+    height: 8,
     position: "absolute",
     bottom: 0,
     backgroundColor: "transparent",
